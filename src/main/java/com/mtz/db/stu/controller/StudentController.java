68eb083c89416c0986ecac7861c14dc60a45466d
basha-github <sharifabee.alur@gmail.com>
2024-06-26 07:37:36 +0530

    coded db

--- src/main/java/com/mtz/db/stu/controller/StudentController.java
+++ src/main/java/com/mtz/db/stu/controller/StudentController.java
@@ -1,8 +1,28 @@
 package com.mtz.db.stu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mtz.db.stu.model.Student;
+import com.mtz.db.stu.service.StudentService;
 
 @RestController
 public class StudentController {
+	
+	//StudentService stuService = new StudentService();
+	
+	@Autowired
+	StudentService stuService;
+	
+	
+	@PostMapping("/mtz/stu/add")
+	public Student addNewStudent(@RequestBody Student stu){
+		
+		return stuService.addNewStudentRecord(stu);
+		
+	}
 
 }
--- src/main/java/com/mtz/db/stu/service/StudentService.java
+++ src/main/java/com/mtz/db/stu/service/StudentService.java
@@ -1,8 +1,20 @@
 package com.mtz.db.stu.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mtz.db.stu.model.Student;
+import com.mtz.db.stu.repo.StudentRepo;
 
 @Service
 public class StudentService {
+	
+	@Autowired
+	StudentRepo stuRepo;
+
+	public Student addNewStudentRecord(Student stu) {
+		
+		return stuRepo.save(stu);
+	}
 
 }
